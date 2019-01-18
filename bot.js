26206524bd457f646356e914366bd91ee8063370
a7axD <44978645+a7axD@users.noreply.github.com>
2019-01-18 20:20:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -612,7 +612,7 @@ if(message.content.startsWith(prefix + "daily")) {
   if(profile[message.author.id].lastDaily != moment().format('second')) {
    profile[message.author.id].lastDaily = moment().format('second')
    profile[message.author.id].credits += 1000000
-    message.channel.send(`:atm: |**${message.author.username} you collect your \`310\` :yen: daily credits!**`)
+    message.channel.send(`:atm: |**${message.author.username} you collect your \`1000000\` :yen: daily credits!**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
